93c0a257aefb387d4e3bdaaaaf33a114ae6ae83e
Christopher Donner <christopher.donner@gmail.com>
2020-03-24 10:26:10 -0400

    basic working marionette-bootstrap thing

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-MyApp = new BackBone.Marionette.Application();
+$(document).ready(function(){
+// MyApp = new BackBone.Marionette.Application();
+
+let basicModel=Backbone.Model.extend({
+    defaults: {
+        text: 'shit'
+    },
+    // constructor: function basicModel(arguments, options) {
+    //     Backbone.Model.prototype.constructor.apply
+    // }
+})
+
+let basicView  = Backbone.View.extend({
+    render: function(){
+        $(this.el).html(_.template($('#template').html(), {simple: this.model.get('text')}))
+    }
+});
+
+let myBasicModel = new basicModel({text: 'poop'})
+
+let myBasicView=new basicView({model: myBasicModel, el: '.region'})
+
+myBasicView.render()
+})
